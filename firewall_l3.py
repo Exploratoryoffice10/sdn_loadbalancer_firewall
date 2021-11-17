e1d62b8de5da26ab7b1b13c37ca351109d9db26d
A.H.V.ABHINAV <abhi.ahv@gmail.com>
2021-11-17 23:55:07 +0530

    added L3 firewall

--- firewall_l3.py
+++ firewall_l3.py
@@ -1,17 +1,14 @@
-
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.util import dpid_to_str, str_to_dpid
 from pox.lib.util import str_to_bool
 from pox.lib.addresses import IPAddr
-from pox.lib.packet import ipv4,arp
+from pox.lib.packet import ipv4
 import time,csv
 
 _flood_delay = 0
 
-
 filepath = "~/pox/pox/misc/rules.csv"
-
 
 def read_rule_file(fp):
     rv = list()
@@ -23,24 +20,33 @@ def read_rule_file(fp):
 
 log = core.getLogger()
 
-
 class LearningSwitch (object):
-    def __init__ (self, connection, transparent):
-        # Switch we'll be adding L2 learning switch capabilities to
-        self.connection = connection
+    def __init__ (self, connection, transparent, blocked_protocols = [], blocked_ports = []):
+        self.connection = connection    
         self.transparent = transparent
         self.macToPort = {}
         connection.addListeners(self)
         self.hold_down_expired = _flood_delay == 0
+        self.blocked_protocols = blocked_protocols
+        self.blocked_ports = blocked_ports
+        if len(self.blocked_protocols):
+            log.debug("blocked protocols are %s"," ".join(self.blocked_protocols))
         self.firewall = dict()  # add rules
-        with open(filepath,"r") as file:
-            fr = csv.reader(file)
-            for sp in fr:
-                self.add_rule(IPAddr(sp[0]),IPAddr(sp[1]));
-        
+        self.fw_tcp = [("10.0.0.1", "10.0.0.4")]
+        self.fw_udp = [("10.0.0.2", "10.0.0.6")]
+        self.fw_icmp = [("10.0.0.5", "10.0.0.8")]
+        self.add_rules()
 
-    def add_rule(self,src,dst):
-        self.firewall[(src,dst)] = True
+    def add_rules(self):
+        if 't' in self.blocked_protocols:
+            for src,dst in self.fw_tcp:
+                self.firewall[("tcp", IPAddr(src), IPAddr(dst))] = True
+        if 'u' in self.blocked_protocols:
+            for src,dst in self.fw_udp:
+                self.firewall[("udp", IPAddr(src), IPAddr(dst))] = True
+        if 'i' in self.blocked_protocols:
+            for src,dst in self.fw_icmp:
+                self.firewall[("icmp", IPAddr(src), IPAddr(dst))] = True
 
     def _handle_PacketIn (self, event):
         packet = event.parsed
@@ -78,58 +84,72 @@ class LearningSwitch (object):
 
         self.macToPort[packet.src] = event.port # 1
         
+        # check before dropping packets
         if isinstance(packet.next,ipv4):
             if packet.find('tcp') is not None:  # tcp packets dropped
-                if self.firewall.get((packet.next.srcip,packet.next.dstip),False):
-                    log.debug("blocked packet from %s to %s",packet.next.srcip,packet.next.dstip)
+                if self.firewall.get(("tcp",packet.next.srcip,packet.next.dstip),False):
+                    log.debug("blocked tcp packet from %s to %s",packet.next.srcip,packet.next.dstip)
                     drop()
                     return
-        
-        if not self.transparent: # 2
+            if packet.find('udp') is not None:  # udp packets dropped
+                if self.firewall.get(("udp",packet.next.srcip,packet.next.dstip),False):
+                    log.debug("blocked udp packet from %s to %s",packet.next.srcip,packet.next.dstip)
+                    drop()
+                    return
+            if packet.find('icmp') is not None:  # icmp packets dropped
+                if self.firewall.get(("icmp",packet.next.srcip,packet.next.dstip),False):
+                    log.debug("blocked icmp packet from %s to %s",packet.next.srcip,packet.next.dstip)
+                    drop()
+                    return
+
+            # tcp ports
+            z = packet.find('tcp')
+            if z:
+                if z.srcport in  self.blocked_ports or z.dstport in self.blocked_ports:
+                    log.debug("blocked TCP packet %s <-> %s, ports: %s to %s",z.srcip,z.srcport,z.dstport)
+                    drop()
+                    return
+
+        if not self.transparent:
             if packet.type == packet.LLDP_TYPE or packet.dst.isBridgeFiltered():
-                drop() # 2a
+                drop() 
                 return
         if packet.dst.is_multicast:
-            flood() # 3a
+            flood() 
         else:
-            if packet.dst not in self.macToPort: # 4
-                flood("Port for %s unknown -- flooding" % (packet.dst,)) # 4a
+            if packet.dst not in self.macToPort: 
+                flood("Port for %s unknown -- flooding" % (packet.dst,)) 
             else:
                 port = self.macToPort[packet.dst]
-                if port == event.port: # 5
-                # 5a
+                if port == event.port: 
                     log.warning("Same port for packet from %s -> %s on %s.%s.  Drop."
                         % (packet.src, packet.dst, dpid_to_str(event.dpid), port))
                     drop(10)
-                return
-            # 6
-            log.debug("installing flow for %s.%i -> %s.%i" %
-                    (packet.src, event.port, packet.dst, port))
-            msg = of.ofp_flow_mod()
-            msg.match = of.ofp_match.from_packet(packet, event.port)
-            msg.idle_timeout = 10
-            msg.hard_timeout = 30
-            msg.actions.append(of.ofp_action_output(port = port))
-            msg.data = event.ofp # 6a
-            self.connection.send(msg)
+                    return
+                log.debug("installing flow for %s.%i -> %s.%i" %
+                        (packet.src, event.port, packet.dst, port))
+                msg = of.ofp_flow_mod()
+                msg.match = of.ofp_match.from_packet(packet, event.port)
+                msg.idle_timeout = 10
+                msg.hard_timeout = 30
+                msg.actions.append(of.ofp_action_output(port = port))
+                msg.data = event.ofp # 6a
+                self.connection.send(msg)
 
 
 class l2_learning (object):
-    def __init__ (self, transparent, ignore = None):
+    def __init__ (self, transparent, blocked_protocols = [], blocked_ports = []):
         core.openflow.addListeners(self)
         self.transparent = transparent
-        self.ignore = set(ignore) if ignore else ()
+        self.blocked_protocols = blocked_protocols
+        self.blocked_ports = blocked_ports
 
     def _handle_ConnectionUp (self, event):
-        if event.dpid in self.ignore:
-            log.debug("Ignoring connection %s" % (event.connection,))
-            return
         log.debug("Connection %s" % (event.connection,))
-        LearningSwitch(event.connection, self.transparent)
+        LearningSwitch(event.connection, self.transparent,self.blocked_protocols,self.blocked_ports)
 
 
-def launch (transparent=False, hold_down=_flood_delay, ignore = None):
-    # starts an l2_learning switch
+def launch (transparent=False, hold_down=_flood_delay, blocked_protocols = None, blocked_ports = None):
     try:
         global _flood_delay
         _flood_delay = int(str(hold_down), 10)
@@ -137,8 +157,15 @@ def launch (transparent=False, hold_down=_flood_delay, ignore = None):
     except:
         raise RuntimeError("Expected hold-down to be a number")
 
-    if ignore:
-        ignore = ignore.replace(',', ' ').split()
-        ignore = set(str_to_dpid(dpid) for dpid in ignore)
+    if blocked_protocols:
+        blocked_protocols = list(blocked_protocols)
+    else:
+        blocked_protocols = []
 
-    core.registerNew(l2_learning, str_to_bool(transparent), ignore)
+    if blocked_ports:
+        blocked_ports = blocked_ports.split(",")
+        blocked_ports = [int(x) for x in blocked_ports]
+    else:
+        blocked_ports = []
+
+    core.registerNew(l2_learning, str_to_bool(transparent), blocked_protocols, blocked_ports)
